5ff6f80dde82eb65af2ca3f826e1f8440dbb87b2
Benjamin Perraudin <bperraud@student.s19.be>
2022-03-20 23:47:46 +0100

    copyfile

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bperraud <bperraud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 18:38:08 by bperraud          #+#    #+#             */
-/*   Updated: 2022/03/20 23:28:59 by bperraud         ###   ########.fr       */
+/*   Updated: 2022/03/20 23:46:58 by bperraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,31 +21,35 @@ int main(int argc, char **argv, char **envp)
     int	f2;
     int i;
 
-	g_envp = envp;
-
     if (argc < 5)
-        return (-1);
-    
+        return (-1); 
+	
+    g_envp = envp;
     f1 = open(argv[1], O_RDONLY);
+    //f2 = open(argv[1], O_RDWR);
     f2 = open(argv[argc-1], O_CREAT | O_RDWR | O_TRUNC, 0644);
     if (f1 < 0 || f2 < 0)
         return (-1);
     i = 1;
-    
 
-    //while (i++ < argc-3)
-    //{
-    //    //printf("i : %i", i);
-    //    pipex(f1, f2, argv, i);
-    //    close(f1);      // sur ? 
-    //    //f1 = f2;        // pas fermé ? 
-    //    f1 = open("file3", O_CREAT | O_RDWR);       // nom de fichier qui n'existe pas déjà
-    //    copy_file(f2, f1);  // copie f2 dans f1
-    //}
+    //int f3 = open("file3", o_creat | o_rdwr);
+    while (i++ < argc-3)
+    {
+        //printf("i : %i", i);
+        pipex(f1, f2, argv, i);
+        close(f1);      // sur ? 
+        close(f2);
 
+        f2 = open(argv[argc-1], O_RDWR);    
+        f1 = f2;        // pas fermé ? 
+
+        f1 = open("file3", O_CREAT | O_RDWR | O_TRUNC, 0644);       // nom de fichier qui n'existe pas déjà
+        copy_file(f2, f1);  // copie f2 dans f1
+    }
     //pipex(f1, 1);
     //pipex(1, f2);
-   
+    //copy_file(f2, f3);
+
     close(f1);
     close(f2);
     
