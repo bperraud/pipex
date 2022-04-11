dcfa87880216f3be62587581381a16d638ddbf66
bperraud <74benji@live.fr>
2022-04-11 17:39:58 +0200

    ctrl D

--- src/pipex_bonus.c
+++ src/pipex_bonus.c
@@ -47,20 +47,17 @@ static void	copy_file(int f1, int f2)
 int	limiter(char *limiter)
 {
 	int		f1;
-	size_t	n;
 	size_t	len_limiter;
 	char	*buf;
 
 	len_limiter = ft_strlen(limiter);
 	f1 = open(FILE_NAME, O_CREAT | O_RDWR | O_TRUNC, 0644);
-	n = 0;
 	while (1)
 	{
 		write(STDIN_FILENO, "> ", 2);
 		buf = get_next_line(1);
-		n = ft_strlen(buf) - 1;
-		if (n != len_limiter || ft_strncmp(buf, limiter, len_limiter) != 0)
-			write(f1, buf, n + 1);
+		if (buf && (ft_strlen(buf) - 1 != len_limiter || ft_strncmp(buf, limiter, len_limiter) != 0))
+			write(f1, buf, ft_strlen(buf));
 		else
 			break ;
 	}
